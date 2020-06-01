31da22385d43f7278bd12e3b410defebae063f3f
Ekaterina <1988snea@gmail.com>
2020-06-02 01:13:10 +0400

    Dropdowns

--- _ADD_INFO/no widget/js/scripts.js
+++ _ADD_INFO/no widget/js/scripts.js
@@ -1,17 +1,43 @@
 jQuery('document').ready(function($) {
-$('.minus').click(function () {
-var span = $(this).next();
-var span_numb = parseInt(span.text());
-if(span_numb) {
-span.text(--span_numb);
-}
-});
-$('.plus').click(function () {
-var span = $(this).prev();
-var span_numb = parseInt(span.text());
-if(span_numb || span_numb == 0) {
-//alert(++span_numb);
-span.text(++span_numb);
-}
-});
+
+
+	$('.minus').click(function () {
+	let span = $(this).next();
+	let span_numb = parseInt(span.text());
+	if(span_numb) {
+	span.text(--span_numb);
+		switch($(this).attr('data-name')){
+			case 'adults':
+				$(".adults_count").text(span.text());
+				break;
+			case 'children':
+				$(".children_count").text(span.text());
+				break;
+			case 'infants':
+				$(".infants_count").text(span.text());
+				break;		
+			}
+		}
+	});
+
+	$('.plus').on('click', function () {
+	let span = $(this).prev();
+	let span_numb = parseInt(span.text());
+	if(span_numb || span_numb == 0) {
+	span.text(++span_numb);
+
+	switch($(this).attr('data-name')){
+			case 'adults':
+				$(".adults_count").text(span.text());
+				break;
+			case 'children':
+				$(".children_count").text(span.text());
+				break;	
+			case 'infants':
+				$(".infants_count").text(span.text());
+				break;		
+			}
+		}
+	});
+
 });
