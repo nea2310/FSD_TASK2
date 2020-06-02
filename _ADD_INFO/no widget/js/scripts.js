8fef8233d3261e9dc059f68c2bf303454fff4da0
Ekaterina <1988snea@gmail.com>
2020-06-03 00:47:05 +0400

    Dropdowns

--- _ADD_INFO/no widget/js/scripts.js
+++ _ADD_INFO/no widget/js/scripts.js
@@ -1,43 +1,48 @@
 jQuery('document').ready(function($) {
 
 
-	$('.minus').click(function () {
+	$('.dropdown-minus').click(function () {
 	let span = $(this).next();
 	let span_numb = parseInt(span.text());
 	if(span_numb) {
 	span.text(--span_numb);
 		switch($(this).attr('data-name')){
-			case 'adults':
-				$(".adults_count").text(span.text());
+			case 'category-one':
+				$(".category-one-count").text(span.text());
 				break;
-			case 'children':
-				$(".children_count").text(span.text());
+			case 'category-two':
+				$(".category-two-count").text(span.text());
 				break;
-			case 'infants':
-				$(".infants_count").text(span.text());
+			case 'category-three':
+				$(".category-three-count").text(span.text());
 				break;		
 			}
 		}
 	});
 
-	$('.plus').on('click', function () {
+	$('.dropdown-plus').on('click', function () {
 	let span = $(this).prev();
 	let span_numb = parseInt(span.text());
 	if(span_numb || span_numb == 0) {
 	span.text(++span_numb);
 
 	switch($(this).attr('data-name')){
-			case 'adults':
-				$(".adults_count").text(span.text());
+			case 'category-one':
+				$(".category-one-count").text(span.text());
 				break;
-			case 'children':
-				$(".children_count").text(span.text());
+			case 'category-two':
+				$(".category-two-count").text(span.text());
 				break;	
-			case 'infants':
-				$(".infants_count").text(span.text());
+			case 'category-three':
+				$(".category-three-count").text(span.text());
 				break;		
 			}
 		}
 	});
+
+	$('.dropdown-header').on('click', function(){
+	$(this).nextAll().slideToggle(500);
+	});
+
 
 });
