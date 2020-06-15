31577c1886467678bc15004aa48359d731ab7db5
Ekaterina <1988snea@gmail.com>
2020-06-16 01:15:40 +0400

    Calendar

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,59 +1,30 @@
-/*import './calendar.scss';
-import $ from 'jquery';
-
-if (!$.fn.datepicker) {
-  require('jquery-ui/ui/widgets/datepicker');
-  require('jquery-ui/themes/base/datepicker.css');
-}
-*/
-
-import 'jquery-ui/ui/widgets/datepicker';
-import 'jquery-ui/themes/base/datepicker.css';
-
-class Calendar {
-  constructor($component) {
-    this.$component = $component;
-    this.render();
-  }
-
-  render() {
-   // let $day = $('.js-calendar__day', this.$component);
-    let $widget = $('.js-calendar__widget', this.$component);
-
-    $widget.datepicker({
-      changeYear: false,
-    //  altField: $day,
-      altFormat: "dd",
-      firstDay: 1
-    });
-
-    $('.calendar__btn-today', this.$component).on('click', () => {
-      $widget.datepicker('setDate', new Date());
-    });
-  }
-}
-
-const calendarinstance = jQuery('document').ready(function($) {
-  $(() => {
-    $('.js-calendar').each((index, node) => {
-      new Calendar($(node));
-    });
-  });
-});
-
-export { calendarinstance }
 
 /*
-
-export default function renderComponent() {
-  $(() => {
-    $('.js-calendar').each((index, node) => {
-      new Calendar($(node));
-    });
-  });
-}
-
-renderComponent();
+это не работает:
+import 'jquery-ui/ui/widgets/datepicker';
+import 'jquery-ui/themes/base/datepicker.css';*/
 
 
-*/
+import '../../plugins/airdatepicker/datepicker.min.js';
+
+
+const calendar =jQuery('document').ready(function($){
+  $('.datepicker-input').datepicker(
+
+{
+//  inline: true,
+  clearButton: true,                       }
+
+)
+})
+
+
+
+
+/*var calendar = $('.datepicker-input').datepicker().data('datepicker');
+
+calendar.clear();*/
+
+
+
+export { calendar }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import * as $ from 'jquery'
 import Post from '@models/post'
 import './babel'
-
-import './styles/styles.less'
 import './styles/styles.css'
+import './styles/styles.less'
 
+
+/*import './plugins/datepicker.min.css';
+import './plugins/datepicker.min.js';
+*/
 
 import WebpackLogo from '@/assets/webpack-logo.png'
 
@@ -16,7 +19,7 @@ $('pre').html(post.toString())
 import * as dropdown from '@/components/dropdown/dropdown';
 import * as pagination from '@/components/pagination/pagination';
 import * as sliderinstance from '@/components/rangeslider/rangeslider';
-import * as calendarinstance from '@/components/calendar/calendar';
+import * as calendar from '@/components/calendar/calendar';
 
 
 
